98201037883910e0ac747ab5d566734d3bf9267d
Antonin Décimo <antonin@tarides.com>
2021-09-28 15:16:10 +0200

    Apply ocamlformat.0.20.1

--- src/XDGBaseDir.mli
+++ src/XDGBaseDir.mli
@@ -20,16 +20,16 @@
 (********************************************************************************)
 
 (** XDG basedir implementation
-  
-    The XDG basedir specification makes clear where to store configuration, cache 
-    and data files. This a way to maintain a clean $HOME. Locations can be
-    customized through environment variables (XDG_DATA_HOME, XDG_CONFIG_HOME). It 
-    also allows to store files in several places (i.e. system directories).
+
+    The XDG basedir specification makes clear where to store configuration,
+    cache and data files. This a way to maintain a clean [$HOME]. Locations can
+    be customized through environment variables ([XDG_DATA_HOME],
+    [XDG_CONFIG_HOME]). It also allows to store files in several places (i.e.
+    system directories).
 
     @author Sylvain Le Gall
     @see <https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html>
-         XDG base specification
-  *)
+        XDG base specification *)
 
 (** {2 Types} *)
 
@@ -37,111 +37,78 @@ type filename = string
 type dirname = string
 type dirnames = dirname list
 
+type t = {
+  data_home : dirname;  (** $HOME/.local/share *)
+  data_dirs : dirnames;  (** /usr/share *)
+  config_home : dirname;  (** $HOME/.config *)
+  config_dirs : dirnames;  (** /etc/xdg *)
+  cache_home : dirname;  (** $HOME/.cache *)
+}
 (** XDG environment *)
-type t = 
-    {
-      data_home :   dirname;  (** $HOME/.local/share *)
-      data_dirs :   dirnames; (** /usr/share *)
-      config_home : dirname;  (** $HOME/.config *)
-      config_dirs : dirnames; (** /etc/xdg *)
-      cache_home :  dirname;  (** $HOME/.cache *)
-    }
 
 (** {2 Modules and functions} *)
 
-(** Default XDG environment *)
 val default : t
+(** Default XDG environment *)
 
-(** [mkdir_openfile f fn] Create parent directory of [fn] and 
+val mkdir_openfile : (filename -> 'a) -> filename -> 'a
+(** [mkdir_openfile f fn] Create parent directory of [fn] and
     apply [f fn]
   *)
-val mkdir_openfile : (filename -> 'a) -> filename -> 'a
 
-module Data :
-  sig
-    (** In this module, [~xdg_env] allows to override the default
+module Data : sig
+  (** In this module, [~xdg_env] allows to override the default
         XDG environment. If you use [~exists:true], the files/dirs
         are tested for existence.
       *)
-       
-    (** Get the user-specific data directory. *)
-    val user_dir : 
-      ?xdg_env:t -> 
-      ?exists:bool -> unit -> dirname
 
-    (** Get a list of all system-specific data directories. *)
-    val system_dirs :
-      ?xdg_env:t -> 
-      ?exists:bool -> unit -> dirname list
+  val user_dir : ?xdg_env:t -> ?exists:bool -> unit -> dirname
+  (** Get the user-specific data directory. *)
 
-    (** Get a list of all data directories. *)
-    val all_dirs : 
-      ?xdg_env:t -> 
-      ?exists:bool -> unit -> dirname list
+  val system_dirs : ?xdg_env:t -> ?exists:bool -> unit -> dirname list
+  (** Get a list of all system-specific data directories. *)
 
-    (** Get the user-specific filename for a data file. *) 
-    val user_file :
-      ?xdg_env:t -> 
-      ?exists:bool -> filename -> filename
+  val all_dirs : ?xdg_env:t -> ?exists:bool -> unit -> dirname list
+  (** Get a list of all data directories. *)
 
-    (** Get the list of system-specific filenames for a data file. *) 
-    val system_files :
-      ?xdg_env:t ->
-      ?exists:bool -> filename -> filename list
+  val user_file : ?xdg_env:t -> ?exists:bool -> filename -> filename
+  (** Get the user-specific filename for a data file. *)
 
-    (** Get the list of all filenames for a specific data file. *) 
-    val all_files :
-      ?xdg_env:t ->
-      ?exists:bool -> filename -> filename list
-  end
+  val system_files : ?xdg_env:t -> ?exists:bool -> filename -> filename list
+  (** Get the list of system-specific filenames for a data file. *)
 
-module Config :
-  sig
-    (** See {!Data} for explanations about [~xdg_env] and [~exists] *)
+  val all_files : ?xdg_env:t -> ?exists:bool -> filename -> filename list
+  (** Get the list of all filenames for a specific data file. *)
+end
 
-    (** Get the user-specific configuration directory. *)
-    val user_dir : 
-      ?xdg_env:t -> 
-      ?exists:bool -> unit -> dirname
+module Config : sig
+  (** See {!Data} for explanations about [~xdg_env] and [~exists] *)
 
-    (** Get a list of all system-specific configuration directories. *)
-    val system_dirs :
-      ?xdg_env:t -> 
-      ?exists:bool -> unit -> dirname list
+  val user_dir : ?xdg_env:t -> ?exists:bool -> unit -> dirname
+  (** Get the user-specific configuration directory. *)
 
-    (** Get a list of all configuration directories. *)
-    val all_dirs : 
-      ?xdg_env:t -> 
-      ?exists:bool -> unit -> dirname list
+  val system_dirs : ?xdg_env:t -> ?exists:bool -> unit -> dirname list
+  (** Get a list of all system-specific configuration directories. *)
 
-    (** Get the user-specific filename for a configuration file. *) 
-    val user_file :
-      ?xdg_env:t -> 
-      ?exists:bool -> filename -> filename
+  val all_dirs : ?xdg_env:t -> ?exists:bool -> unit -> dirname list
+  (** Get a list of all configuration directories. *)
 
-    (** Get the list of system-specific filenames for a configuration file. *) 
-    val system_files :
-      ?xdg_env:t ->
-      ?exists:bool -> filename -> filename list
+  val user_file : ?xdg_env:t -> ?exists:bool -> filename -> filename
+  (** Get the user-specific filename for a configuration file. *)
 
-    (** Get the list of all filenames for a specific configuration file. *) 
-    val all_files :
-      ?xdg_env:t ->
-      ?exists:bool -> filename -> filename list
-  end
+  val system_files : ?xdg_env:t -> ?exists:bool -> filename -> filename list
+  (** Get the list of system-specific filenames for a configuration file. *)
 
-module Cache :
-  sig
-    (** See {!Data} for explanations about [~xdg_env] and [~exists] *)
+  val all_files : ?xdg_env:t -> ?exists:bool -> filename -> filename list
+  (** Get the list of all filenames for a specific configuration file. *)
+end
 
-    (** Get the user-specific cache directory. *)
-    val user_dir : 
-      ?xdg_env:t -> 
-      ?exists:bool -> unit -> dirname
+module Cache : sig
+  (** See {!Data} for explanations about [~xdg_env] and [~exists] *)
 
-    (** Get the user-specific filename for a cache file. *) 
-    val user_file :
-      ?xdg_env:t -> 
-      ?exists:bool -> filename -> filename
-  end
+  val user_dir : ?xdg_env:t -> ?exists:bool -> unit -> dirname
+  (** Get the user-specific cache directory. *)
 
+  val user_file : ?xdg_env:t -> ?exists:bool -> filename -> filename
+  (** Get the user-specific filename for a cache file. *)
+end
--- test/test.ml
+++ test/test.ml
@@ -25,87 +25,56 @@ open FilePath
 open XDGBaseDir
 
 let mk_tmpdir () =
-  let tmpdir =
-    Filename.temp_file "xdg-basedir" ".dir"
-  in
-  rm [tmpdir];
+  let tmpdir = Filename.temp_file "xdg-basedir" ".dir" in
+  rm [ tmpdir ];
   mkdir tmpdir;
   tmpdir
 
 let bracket_xdg_env test_ctxt f =
   let home, sys1, sys2 =
     bracket
-      (fun _test_ctxt ->
-         mk_tmpdir (),
-         mk_tmpdir (),
-         mk_tmpdir ())
-      (fun (dn1, dn2, dn3) _test_ctxt ->
-         rm ~recurse:true [dn1; dn2; dn3])
-      test_ctxt in
+      (fun _test_ctxt -> (mk_tmpdir (), mk_tmpdir (), mk_tmpdir ()))
+      (fun (dn1, dn2, dn3) _test_ctxt -> rm ~recurse:true [ dn1; dn2; dn3 ])
+      test_ctxt
+  in
   let mk_fn = make_filename in
   let xdg_env =
     {
-      data_home = mk_fn [home; ".local"; "share"];
-      data_dirs = [mk_fn [sys1; "share"];
-                   mk_fn [sys2; "share"]];
-      config_home = mk_fn [home; ".config"];
-      config_dirs = [mk_fn [sys1; "etc"];
-                     mk_fn [sys2; "etc"]];
-      cache_home = mk_fn [home; ".cache"];
+      data_home = mk_fn [ home; ".local"; "share" ];
+      data_dirs = [ mk_fn [ sys1; "share" ]; mk_fn [ sys2; "share" ] ];
+      config_home = mk_fn [ home; ".config" ];
+      config_dirs = [ mk_fn [ sys1; "etc" ]; mk_fn [ sys2; "etc" ] ];
+      cache_home = mk_fn [ home; ".cache" ];
     }
   in
   f xdg_env
 
 let test_of_vector (nm, user_file, all_files) =
-  nm>:: fun test_ctxt ->
-    bracket_xdg_env test_ctxt
-      (fun xdg_env ->
-         let xdg_env =
-           Some xdg_env
-         in
-         let fn =
-           user_file ?xdg_env ?exists:(Some false) "test.data"
-         in
-         let assert_all_files ~msg exp =
-           assert_equal
-             ~msg
-             ~printer:(String.concat "; ")
-             exp
-             (all_files ?xdg_env ?exists:(Some true) "test.data")
-         in
-         assert_raises
-           ~msg:"user file doesn't exist"
-           Not_found
-           (fun () ->
-              user_file ?xdg_env ?exists:(Some true) "test.data");
-         assert_all_files
-           ~msg:"nothing"
-           [];
-         mkdir_openfile touch fn;
-         assert_all_files
-           ~msg:"1 file"
-           [fn])
+  nm >:: fun test_ctxt ->
+  bracket_xdg_env test_ctxt (fun xdg_env ->
+      let xdg_env = Some xdg_env in
+      let fn = user_file ?xdg_env ?exists:(Some false) "test.data" in
+      let assert_all_files ~msg exp =
+        assert_equal ~msg ~printer:(String.concat "; ") exp
+          (all_files ?xdg_env ?exists:(Some true) "test.data")
+      in
+      assert_raises ~msg:"user file doesn't exist" Not_found (fun () ->
+          user_file ?xdg_env ?exists:(Some true) "test.data");
+      assert_all_files ~msg:"nothing" [];
+      mkdir_openfile touch fn;
+      assert_all_files ~msg:"1 file" [ fn ])
 
 let tests =
-  "XDGBaseDir">:::
-  (List.map test_of_vector
-     [
-       "data_home",
-       Data.user_file,
-       Data.all_files;
+  "XDGBaseDir"
+  >::: List.map test_of_vector
+         [
+           ("data_home", Data.user_file, Data.all_files);
+           ("config_home", Config.user_file, Config.all_files);
+           ( "cache_home",
+             Cache.user_file,
+             fun ?xdg_env ?exists fn ->
+               try [ Cache.user_file ?xdg_env ?exists fn ]
+               with Not_found -> [] );
+         ]
 
-       "config_home",
-       Config.user_file,
-       Config.all_files;
-
-       "cache_home",
-       Cache.user_file,
-       (fun ?xdg_env ?exists fn ->
-          try
-            [Cache.user_file ?xdg_env ?exists fn]
-          with Not_found ->
-            []);
-     ])
-
-let () =
-  run_test_tt_main tests
+let () = run_test_tt_main tests
